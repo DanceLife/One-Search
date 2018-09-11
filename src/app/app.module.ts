a67d1548c4cbf56995fc9b2c6dafb2a65a182433
Hector Higuera <hectorhiguera@outlook.com>
2018-09-10 20:48:32 -0400

    Removing Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,7 @@ import { AuthModule } from './auth/auth.module';
 import { OptionsComponent } from './options/options.component';
 import { EnvironmentComponent } from './environment/environment.component';
 import { SearchComponent } from './search/search.component';
-import { ActionComponent } from './actions/action/action.component';
-import { AnotherActionComponent } from './actions/another-action/another-action.component';
-import { OneMoreActionComponent } from './actions/one-more-action/one-more-action';
-import { ActionsComponent } from './actions/actions.component';
 import { HttpModule } from '@angular/http';
-import { ArticlesComponent } from './articles/articles.component';
 import { GardeningComponent } from './gardening/gardening.component';
 import { SpaceComponent } from './space/space.component';
 import { DancingComponent } from './dancing/dancing.component';
@@ -21,12 +16,7 @@ import { DancingComponent } from './dancing/dancing.component';
     AppComponent,
     OptionsComponent,
     EnvironmentComponent,
-    ActionsComponent,
-    ActionComponent,
-    AnotherActionComponent,
-    OneMoreActionComponent,
     SearchComponent,
-    ArticlesComponent,
     GardeningComponent,
     SpaceComponent,
     DancingComponent,
